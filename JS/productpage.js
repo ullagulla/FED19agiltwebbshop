b2262ff1cd224c484f562ee8fcd9963743b126df
Louise Dennevi <louisedennevi@Louises-MBP.mediegruppen.se>
2019-12-09 13:53:55 +0100

    tab

--- JS/productpage.js
+++ JS/productpage.js
@@ -1,17 +1,12 @@
 $(document).ready(function(){
 
-    function openCity(evt, cityName) {
-        var i, tabcontent, tablinks;
-        tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-          tabcontent[i].style.display = "none";
-        }
-        tablinks = document.getElementsByClassName("tablinks");
-        for (i = 0; i < tablinks.length; i++) {
-          tablinks[i].className = tablinks[i].className.replace(" active", "");
+    function openCity(cityName) {
+        var i;
+        var x = document.getElementsByClassName("city");
+        for (i = 0; i < x.length; i++) {
+          x[i].style.display = "none";
         }
         document.getElementById(cityName).style.display = "block";
-        evt.currentTarget.className += " active";
       }
 
 });
