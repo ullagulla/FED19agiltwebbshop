519713d5782fae8b3100e93650b180b47ed294a5
Louise Dennevi <louisedennevi@Louises-MBP.mediegruppen.se>
2019-12-10 14:12:23 +0100

    modal

---
+++ JS/kassa.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#examplebtnModal').click(function(){
+      $('#exampleModal').modal({
+        backdrop: 'static',
+        keyboard: false
+      });
+    });
+  });
